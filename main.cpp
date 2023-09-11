21e36329a276d581b7220a6ffab2f4ef1561c4a1
NEKGod <1559096467@qq.com>
2023-09-11 13:51:10 +0800

    update

--- main.cpp
+++ main.cpp
@@ -10,44 +10,6 @@
 #include <ShlObj.h>
 #include <fstream>
 
-bool WindowsAddToStartup(const std::wstring& executablePath) {
-    HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-    if (SUCCEEDED(hr)) {
-        wchar_t startupPath[MAX_PATH];
-        if (SUCCEEDED(SHGetFolderPathW(nullptr, CSIDL_STARTUP, nullptr, 0, startupPath))) {
-            std::wstring shortcutPath = std::wstring(startupPath) + L"\\" + L"windows_printer.lnk"; // 替换为你的快捷方式名
-
-            IShellLinkW* pShellLink;
-            hr = CoCreateInstance(CLSID_ShellLink, nullptr, CLSCTX_INPROC_SERVER, IID_IShellLinkW, (LPVOID*)&pShellLink);
-            if (SUCCEEDED(hr)) {
-                pShellLink->SetPath(executablePath.c_str());
-                IPersistFile* pPersistFile;
-                hr = pShellLink->QueryInterface(IID_IPersistFile, (LPVOID*)&pPersistFile);
-                if (SUCCEEDED(hr)) {
-                    hr = pPersistFile->Save(shortcutPath.c_str(), TRUE);
-                    pPersistFile->Release();
-                    if (SUCCEEDED(hr)) {
-                        std::wcout << L"已将程序添加至启动文件夹。" << std::endl;
-                        return true;
-                    } else {
-                        std::cerr << "无法保存快捷方式文件。" << std::endl;
-                    }
-                }
-                pShellLink->Release();
-            } else {
-                std::cerr << "无法创建Shell链接实例。" << std::endl;
-            }
-        } else {
-            std::cerr << "无法获取启动文件夹路径。" << std::endl;
-        }
-        CoUninitialize();
-    } else {
-        std::cerr << "无法初始化COM库。" << std::endl;
-    }
-    return false;
-}
-
-
 bool CopyFile(const std::wstring& source, const std::wstring& destination) {
     return CopyFileW(source.c_str(), destination.c_str(), FALSE);
 }
@@ -94,9 +56,6 @@ namespace printer {
     }
 
     void getPrinterList(const FunctionCallbackInfo<Value> &args) {
-        // 设置控制台以支持 UTF-8 编码
-        SetConsoleOutputCP(CP_UTF8);
-
         Isolate *isolate = args.GetIsolate();
         // 创建一个新的V8对象
         Local<Context> context = isolate->GetCurrentContext();
@@ -120,6 +79,7 @@ namespace printer {
                 printList->Set(context, i, obj);
             }
         }
+//        GlobalFree(pPrinterInfo);
         FUNCTION_RETURN(args, printList)
     }
 
@@ -139,31 +99,39 @@ namespace printer {
         LONG ret = 0;
 
         v8::String::Utf8Value argsPrinterName(args.GetIsolate(), args[0]);
-        const char* argsPrinterNamePoint = *argsPrinterName;
+        const char *argsPrinterNamePoint = *argsPrinterName;
         // 将宽字符字符串赋值给LPWSTR类型
         LPWSTR printerName = ConvertToLPWSTR(argsPrinterNamePoint);
-        WCHAR* printerNameW = printerName;
+        WCHAR *printerNameW = printerName;
         BOOL ok = OpenPrinterW(printerName, &hPrinter, nullptr);
         if (!ok) {
+            ClosePrinter(hPrinter);
             TRY_FUNCTION(isolate, "打印机名称不存在 001!!!")
         }
         GetPrinterW(hPrinter, 2, nullptr, 0, &needed);
-        BYTE* printerBuffer  = new BYTE[needed];
+        BYTE *printerBuffer = new BYTE[needed];
         ok = GetPrinterW(hPrinter, 2, printerBuffer, needed, &needed);
         if (!ok || needed <= sizeof(PRINTER_INFO_2)) {
+            ClosePrinter(hPrinter);
+            delete[] printerBuffer;
             TRY_FUNCTION(isolate, "获取打印机信息异常 001!!!")
         }
-        auto* info = reinterpret_cast<PRINTER_INFO_2*>(printerBuffer);
+        auto *info = reinterpret_cast<PRINTER_INFO_2 *>(printerBuffer);
         Local<Object> printerInfoNodeObject = mapperPrinterInfoNodeObjectW(isolate, context, info);
         /* ask for the size of DEVMODE struct */
         structSize = DocumentPropertiesW(nullptr, hPrinter, printerName, nullptr, nullptr, 0);
         if (structSize < sizeof(DEVMODEW)) {
+            ClosePrinter(hPrinter);
+            delete[] printerBuffer;
             TRY_FUNCTION(isolate, "获取打印机信息异常 002!!!")
         }
-        devMode = (DEVMODEW*)new BYTE[structSize];
+        devMode = (DEVMODEW *)new BYTE[structSize];
         // Get the default DevMode for the printer and modify it for your needs.
         ret = DocumentPropertiesW(nullptr, hPrinter, printerName, devMode, nullptr, DM_OUT_BUFFER);
         if (IDOK != ret) {
+            ClosePrinter(hPrinter);
+            delete[] printerBuffer;
+            delete[] (BYTE *)devMode;
             TRY_FUNCTION(isolate, "获取打印机信息异常 003!!!")
         }
 
@@ -172,48 +140,60 @@ namespace printer {
             DWORD n = DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERS, nullptr, nullptr);
             DWORD n2 = DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERNAMES, nullptr, nullptr);
             DWORD n3 = DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERSIZE, nullptr, nullptr);
+
             if (n != n2 || n != n3 || 0 == n || ((DWORD)-1 == n)) {
+                ClosePrinter(hPrinter);
+                delete[] printerBuffer;
+                delete[] (BYTE *)devMode;
                 TRY_FUNCTION(isolate, "获取打印机纸张类型异常 00010!!!")
             }
+
             OBJECT_SET_NUMBER(context, printerInfoNodeObject, "nPaperSizes", (int)n);
-            int nPaperSizes = (int)n;
+
             size_t paperNameSize = 64;
-            WORD *papers = new WORD[n];
-            auto *paperNamesSeq = new WCHAR[paperNameSize * n + 1]; // +1 is "just in case"
-            auto *paperSizes = new POINT[n];
-            DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERS, (WCHAR*)papers, nullptr);
-            DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERSIZE, (WCHAR*)paperSizes, nullptr);
-            DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERNAMES, paperNamesSeq, nullptr);
+
+            std::unique_ptr<WORD[]> papers(new WORD[n]);
+            std::unique_ptr<WCHAR[]> paperNamesSeq(new WCHAR[paperNameSize * n + 1]); // +1 is "just in case"
+            std::unique_ptr<POINT[]> paperSizes(new POINT[n]);
+
+            DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERS, (WCHAR *)papers.get(), nullptr);
+            DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERSIZE, (WCHAR *)paperSizes.get(), nullptr);
+            DeviceCapabilitiesW(printerNameW, nullptr, DC_PAPERNAMES, paperNamesSeq.get(), nullptr);
+
             Local<Array> paperNames = Array::New(isolate);
-            WCHAR* paperName = paperNamesSeq;
+            WCHAR *paperName = paperNamesSeq.get();
+
             for (int i = 0; i < (int)n; i++) {
                 paperNames->Set(context, i, String::NewFromUtf8(isolate, WideCharToNarrow(paperName)).ToLocalChecked());
                 paperName += paperNameSize;
             }
+
             OBJECT_SET_ARRAY(context, printerInfoNodeObject, "paperNames", paperNames)
         }
-
+//
         {
             DWORD n = DeviceCapabilitiesW(printerNameW, nullptr, DC_BINS, nullptr, nullptr);
             DWORD n2 = DeviceCapabilitiesW(printerNameW, nullptr, DC_BINNAMES, nullptr, nullptr);
             if (n != n2 || ((DWORD)-1 == n)) {
+                ClosePrinter(hPrinter);
+                delete[] printerBuffer;
+                delete[] (BYTE *)devMode;
                 TRY_FUNCTION(isolate, "获取打印机纸张类型异常 0012!!!")
             }
-//            printer->nBins = n;
+            std::cout << n << std::endl;
             if (n > 0) {
                 size_t binNameSize = 24;
-                WORD * bins = new WORD(n);
-                auto* binNamesSeq = new WCHAR[binNameSize * n + 1];
-                DeviceCapabilitiesW(printerNameW, nullptr, DC_BINS, (WCHAR*)bins, nullptr);
-                DeviceCapabilitiesW(printerNameW, nullptr, DC_BINNAMES, binNamesSeq, nullptr);
-                WCHAR* binName = binNamesSeq;
+                std::unique_ptr<WORD[]> bins(new WORD[n]);
+                std::unique_ptr<WCHAR[]> binNamesSeq(new WCHAR[binNameSize * n + 1]);
+                DeviceCapabilitiesW(printerNameW, nullptr, DC_BINS, (WCHAR *)bins.get(), nullptr);
+                DeviceCapabilitiesW(printerNameW, nullptr, DC_BINNAMES, binNamesSeq.get(), nullptr);
+                WCHAR *binName = binNamesSeq.get();
                 Local<Array> binNames = Array::New(isolate);
                 for (int i = 0; i < (int)n; i++) {
                     binNames->Set(context, i, String::NewFromUtf8(isolate, WideCharToNarrow(binName)).ToLocalChecked());
                     binName += binNameSize;
                 }
                 OBJECT_SET_ARRAY(context, printerInfoNodeObject, "binNames", binNames)
-                delete[] binNamesSeq;
             }
         }
 
@@ -235,9 +215,12 @@ namespace printer {
             n = DeviceCapabilitiesW(printerName, nullptr, DC_ORIENTATION, nullptr, nullptr);
             OBJECT_SET_BOOL(context, printerInfoNodeObject, "orientation", (n > 0))
         }
-
+        ClosePrinter(hPrinter);
+        delete[] printerBuffer;
+        delete[] (BYTE *)devMode;
         FUNCTION_RETURN(args, printerInfoNodeObject)
     }
+
 
     void getPrinterJobList(const FunctionCallbackInfo<Value> &args) {
         Isolate *isolate = args.GetIsolate();
@@ -249,7 +232,7 @@ namespace printer {
         Local<Context> context = isolate->GetCurrentContext();
         // 创建一个新的V8数组
         Local<Array> jobList = Array::New(isolate);
-        const char* argsPrinterNamePoint = *argsPrinterName;
+        const char *argsPrinterNamePoint = *argsPrinterName;
         HANDLE hPrinter = nullptr;
         BOOL ok = OpenPrinter(ConvertUtf8ToGbk(argsPrinterNamePoint), &hPrinter, nullptr);
         if (!ok) {
@@ -261,16 +244,15 @@ namespace printer {
         if (dwBytesNeededJob < 0) {
             TRY_FUNCTION(isolate, "打印机任务获取错误!!! 001")
         }
-        JOB_INFO_1* pJobInfo = nullptr;
-        pJobInfo = static_cast<JOB_INFO_1*>(malloc(dwBytesNeededJob));
+        JOB_INFO_1 *pJobInfo = nullptr;
+        pJobInfo = static_cast<JOB_INFO_1 *>(malloc(dwBytesNeededJob));
         if (!pJobInfo) {
             TRY_FUNCTION(isolate, "打印机任务获取错误!!! 002")
         }
-        if (EnumJobs(hPrinter, 0, 100, 1, reinterpret_cast<BYTE*>(pJobInfo), dwBytesNeededJob, &dwBytesNeededJob, &dwJobCount)) {
+        if (EnumJobs(hPrinter, 0, 100, 1, reinterpret_cast<BYTE *>(pJobInfo), dwBytesNeededJob, &dwBytesNeededJob,
+                     &dwJobCount)) {
             for (DWORD j = 0; j < dwJobCount; ++j) {
                 Local<Object> obj = Object::New(isolate);
-                std::cout << "Job ID: " << pJobInfo[j].JobId << std::endl;
-                std::cout << "Job Status: " << pJobInfo[j].Status << std::endl;
                 OBJECT_SET_NUMBER(context, obj, "JobId", pJobInfo[j].JobId)
                 OBJECT_SET_STRING(context, obj, "pPrinterName", gbkStringToUtf8String(pJobInfo[j].pPrinterName).c_str())
                 OBJECT_SET_STRING(context, obj, "pMachineName", pJobInfo[j].pMachineName)
@@ -291,18 +273,20 @@ namespace printer {
         FUNCTION_RETURN(args, jobList)
     }
 
-    void AddToStartup(const FunctionCallbackInfo<Value> &args){
+    void AddToStartup(const FunctionCallbackInfo<Value> &args) {
         Isolate *isolate = args.GetIsolate();
         // 创建一个新的V8对象
         Local<Context> context = isolate->GetCurrentContext();
         v8::String::Utf8Value argsPrinterName(args.GetIsolate(), args[0]);
-        const char* filePath_cstr = *argsPrinterName;
+        const char *filePath_cstr = *argsPrinterName;
         // 将宽字符字符串赋值给LPWSTR类型
         LPWSTR filePath = ConvertToLPWSTR(filePath_cstr);
-        FUNCTION_RETURN(args, v8::Boolean::New(isolate,  WindowsAddToStartupBat(filePath, filePath_cstr)))
+        FUNCTION_RETURN(args, v8::Boolean::New(isolate, WindowsAddToStartupBat(filePath, filePath_cstr)))
     }
 
     void Initialize(Local<Object> exports) {
+        // 设置控制台以支持 UTF-8 编码
+        SetConsoleOutputCP(CP_UTF8);
         NODE_SET_METHOD(exports, "hello", Method);
         NODE_SET_METHOD(exports, "getPrinterList", getPrinterList);
         NODE_SET_METHOD(exports, "getPrinterInfo", getPrinterInfo);
@@ -314,5 +298,4 @@ namespace printer {
 
     NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
 
-}  // namespace demo
-
+}
